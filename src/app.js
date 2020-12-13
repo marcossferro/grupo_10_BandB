85dc7efc6f90cc7c0b799c132e15c63f5ea6691c
Maximiliano <maximilianoalbeldas@yahoo.com.ar>
2020-12-13 13:59:30 -0300

    Modificamos el nombre del html product, agregamos modificaciones a app.js

--- src/app.js
+++ src/app.js
@@ -2,31 +2,35 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./public')); 
+app.use('/public/css', express.static(__dirname + '/css'));
+app.use('/src', express.static(__dirname + '/src'));
+app.use('/public/img', express.static(__dirname + '/img'));
+// app.use(express.static('./src')); 
 // ó tambien... app.use(express.static(path.join(dirname, './public') ));
 
 app.get('/', function(req,res){
-    res.sendFile( path.join(dirname, '') ); // path te da una ruta absoluta
+    res.sendFile(__dirname + "/views/home/index.html" ); // path te da una ruta absoluta
 })
 
 app.get('/login', function(req,res){
-    res.sendFile( path.join(dirname, './views/login.html') ); 
+    res.sendFile(__dirname + '/views/login/login.html'); 
 })
 
 app.get('/register', function(req,res){
-    res.sendFile( path.join(dirname, './views/register.html') );
+    res.sendFile(__dirname + '/views/register/register.html');
 })
 
 app.get('/product', function(req,res){
-    res.sendFile( path.join(dirname, './views/product.html') );
+    res.sendFile(__dirname + '/views/product/product.html');
 })
 
 app.get('/productCart', function(req,res){
-    res.sendFile( path.join(dirname, './views/productCart.html') );
+    res.sendFile(__dirname + '/views/productCart/productCart.html');
 })
 
 
 let puerto = 3000;
 app.listen(puerto, function(){
     console.log('El servidor esta corriendo en el puerto '+ puerto);
+    console.log("http://localhost:3000")
 });
