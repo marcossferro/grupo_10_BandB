790501ce532aadce4c54ec7a05258a40cc111d5a
Maximiliano <maximilianoalbeldas@yahoo.com.ar>
2021-03-05 21:08:42 -0300

    Modificamos archivos de css y ejs

--- public/js/front.js
+++ public/js/front.js
@@ -1,15 +1,124 @@
-window.addEventListener("load", function(e){
-    e.preventDefault()
-    
-    let deleteButton = document.getElementById("deleteButton")
+window.addEventListener("load", function(){
 
-deleteButton.addEventListener("click", function(e){
-    if(confirm("¿Seguro que quieres eliminar este producto?")){
-        e.preventDefault()
-        alert("El producto fue eliminado")
-    }else{
-        e.preventDefault()
-        alert("El producto no fue eliminado")
-    }
-})
-})
+  // Funcion para el banner
+    var myIndex = 0;
+    carousel();
+    
+    function carousel() {
+      var i;
+      var imagen = document.getElementsByClassName("imagenSlider");
+      for (i = 0; i < imagen.length; i++) {
+        imagen[i].style.display = "none";  
+      }
+      myIndex++;
+      if (myIndex > imagen.length) {myIndex = 1}    
+      imagen[myIndex-1].style.display = "block";  
+      setTimeout(carousel, 2000); // Cambia la imagen cada 2 segundos
+    };
+
+    // Funcion para la barra de busqueda
+    let searchIcon = document.getElementById("searchIcon");
+    let searchBar = document.getElementById("searchBar");
+    
+    searchIcon.addEventListener("click", function(){
+      if(searchBar.style.display == "block"){
+        searchBar.style.display = "none"
+      }else{
+        searchBar.style.display = "block"
+      }
+    });
+    
+    // Funcion para borrar el value por defecto de la barra de busqueda
+    let valueInput = document.getElementById("search");
+
+    valueInput.addEventListener("click", function(){
+      valueInput.value = "";
+      valueInput.style.color = "black"
+    })
+
+    // Funcion para carrete de productos destacados Tablet
+
+    let flechaIzq = document.getElementById("flechaIzq");
+    let flechaDer = document.getElementById("flechaDer");
+    
+    let destacadosUno = document.getElementById("productosDestacados1");
+    let destacadosDos = document.getElementById("productosDestacados2");
+    let destacadosTres = document.getElementById("productosDestacados3");
+
+
+    flechaDer.addEventListener("click", function(){
+      if(destacadosUno.style.display == "flex"){
+        destacadosUno.style.display = "none"
+        destacadosDos.style.display = "flex"
+        destacadosTres.style.display = "none"
+      }else if(destacadosDos.style.display == "flex"){
+        destacadosUno.style.display = "none"
+        destacadosDos.style.display = "none"
+        destacadosTres.style.display = "flex"
+      }else{
+        destacadosUno.style.display = "flex"
+        destacadosDos.style.display = "none"
+        destacadosTres.style.display = "none"
+      }
+
+    });
+
+    flechaIzq.addEventListener("click", function(){
+      if(destacadosUno.style.display == "flex"){
+        destacadosUno.style.display = "none"
+        destacadosDos.style.display = "none"
+        destacadosTres.style.display = "flex"
+      }else if(destacadosDos.style.display == "flex"){
+        destacadosUno.style.display = "flex"
+        destacadosDos.style.display = "none"
+        destacadosTres.style.display = "none"
+      }else{
+        destacadosUno.style.display = "none"
+        destacadosDos.style.display = "flex"
+        destacadosTres.style.display = "none"
+      }
+    });
+    
+    // Funcion para carrete de productos destacados Desktop
+
+    let flechaIzqDesk = document.getElementById("flechaIzqDesk");
+    let flechaDerDesk = document.getElementById("flechaDerDesk");
+    
+    let destacadosUnoDesk = document.getElementById("productosDestacados1Desktop");
+    let destacadosDosDesk = document.getElementById("productosDestacados2Desktop");
+    let destacadosTresDesk = document.getElementById("productosDestacados3Desktop");
+
+
+    flechaDerDesk.addEventListener("click", function(){
+      if(destacadosUnoDesk.style.display == "flex"){
+        destacadosUnoDesk.style.display = "none"
+        destacadosDosDesk.style.display = "flex"
+        destacadosTresDesk.style.display = "none"
+      }else if(destacadosDosDesk.style.display == "flex"){
+        destacadosUnoDesk.style.display = "none"
+        destacadosDosDesk.style.display = "none"
+        destacadosTresDesk.style.display = "flex"
+      }else{
+        destacadosUnoDesk.style.display = "flex"
+        destacadosDosDesk.style.display = "none"
+        destacadosTresDesk.style.display = "none"
+      }
+
+    });
+
+    flechaIzqDesk.addEventListener("click", function(){
+      if(destacadosUnoDesk.style.display == "flex"){
+        destacadosUnoDesk.style.display = "none"
+        destacadosDosDesk.style.display = "none"
+        destacadosTresDesk.style.display = "flex"
+      }else if(destacadosDosDesk.style.display == "flex"){
+        destacadosUnoDesk.style.display = "flex"
+        destacadosDosDesk.style.display = "none"
+        destacadosTresDesk.style.display = "none"
+      }else{
+        destacadosUnoDesk.style.display = "none"
+        destacadosDosDesk.style.display = "flex"
+        destacadosTresDesk.style.display = "none"
+      }
+    });
+});
--- src/controllers/generalController.js
+++ src/controllers/generalController.js
@@ -1,7 +1,11 @@
 const path = require("path")
+const fs = require("fs")
+
+let productos = fs.readFileSync(path.join(__dirname, "../data/productos.json"), "utf8");
+productos = JSON.parse(productos);
 
 module.exports = {
     home: function(req,res){
-        res.render("index")
+        res.render("index", {producto: productos})
     },
 }
