d4a2ff804ee951b2fbfb443a01fdf4d7c2ba5ff1
Maximiliano <maximilianoalbeldas@yahoo.com.ar>
2021-04-14 20:42:37 -0300

    Proyecto React

--- src/routers/api/product.js
+++ src/routers/api/product.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+var cors = require('cors')
+
 
 const apiProductsController = require("../../controllers/api/productController")
 
-router.get('/getProducts', apiProductsController.productos)
-router.get('/getProducts/:id', apiProductsController.productoPorId)
-router.get('/getCategories', apiProductsController.categorias)
-router.get('/getCategoriesWithProducts', apiProductsController.productosPorCategoria)
+router.get('/getProducts', cors(), apiProductsController.productos)
+router.get('/getProducts/:id', cors(), apiProductsController.productoPorId)
+router.get('/getCategories', cors(), apiProductsController.categorias)
+router.get('/getCategoriesWithProducts', cors(), apiProductsController.productosPorCategoria)
 
 module.exports = router
--- src/routers/api/user.js
+++ src/routers/api/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+var cors = require('cors')
+
 const userAPIController = require('../../controllers/api/userController');
 
-router.get('/getUsers', userAPIController.getAll);
-router.get('/getUsers/:id', userAPIController.getById);
+router.get('/getUsers', cors() ,userAPIController.getAll);
+router.get('/getUsers/:id', cors() ,userAPIController.getById);
 
 module.exports = router
