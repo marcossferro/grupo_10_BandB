fe6d09aae898a9968b6bcde0f25b1f2c5eab8180
Maximiliano <maximilianoalbeldas@yahoo.com.ar>
2021-04-23 12:28:21 -0300

    Arreglando Validaciones

--- src/validations/perfilValidator.js
+++ src/validations/perfilValidator.js
@@ -1,8 +1,7 @@
 const { check, validationResult, body } = require ("express-validator");
 const bcrypt = require("bcryptjs");
-const db = require("../database/models")
-
-//  CHEQUEAR
+const db = require("../database/models");
+const { response } = require("express");
 
 module.exports = [
     check("nombre").isLength({min:2}).withMessage("El nombre no puede estar vacio y debera tener al menos 2 caracteres"),
@@ -26,9 +25,24 @@ module.exports = [
         }
     ),
     check("repassword").custom(async(repassword, {req})=>{
+
+      //Guardo el valor de contraseña en variable
       let contraseña = req.body.contraseña
-      if(contraseña !== repassword){
-        throw new Error("La contraseña debe ser la misma")
-      }
-    }).isLength({min: 8}).withMessage("Debe contener al menos 8 caracteres").matches("[0-9]").withMessage("Debe contener al menos un número").matches("[A-Z]").withMessage("Debe contener al menos una letra mayúscula").matches(/[.*+\-?^${}_()|[\]\\]/g,'\\$&').withMessage("Debe contener al menos un caracter especial: .*+\-?^${}_()|[\]\\ ")
+
+      //Guardo las expresiones regulares en variables
+      let regex1 = /[0-9]/;
+      let regex2 = /[A-Z]/;
+      let regex3 = /[.*+\-?^${}_()|[\]\\]/;
+
+      //Testeo (como ejemplo) que repassword contenga alguna de las expresiones regulares. Si contiene todas, en consola, deberia mostrarse true 3 veces y ningun error de validacion en password nueva y repassword.
+      console.log(repassword)
+      console.log(regex1.test(repassword))
+      console.log(regex2.test(repassword))
+      console.log(regex3.test(repassword))
+      
+      //Armo un if donde si se cumplen todas las condiciones de la contraseña devuelve true (ningun error de validacion se muestra), sino devuelve el error de validacion por pantalla con el mensaje en "new Error"
+      if(repassword.length == 0 && contraseña.length == 0 || repassword.length >= 8 && contraseña.length >= 8 && repassword == contraseña && regex1.test(repassword) == true && regex2.test(repassword) == true && regex3.test(repassword) == true){
+        return true
+      } throw new Error("La contraseña debe ser la misma")
+    })
   ]
